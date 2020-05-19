506195cbef703745f86c680112b50abd37878365
Hanjoung Lee <hanjoung.lee@samsung.com>
2020-05-19 13:23:40 +0900

    Update
    ONE-DCO-1.0-Signed-off-by: Hanjoung Lee <hanjoung.lee@samsung.com>

--- runtime/onert/backend/cpu/kernel/AddLayer.cc
+++ runtime/onert/backend/cpu/kernel/AddLayer.cc
@@ -65,8 +65,8 @@ void AddLayer::addQuant8()
   throw std::runtime_error{"NYI"};
 }
 
-void AddLayer::configure(const operand::Tensor *lhs, const operand::Tensor *rhs,
-                         const ir::Activation activation, operand::Tensor *output)
+void AddLayer::configure(const ITensor *lhs, const ITensor *rhs,
+                         const ir::Activation activation, ITensor *output)
 {
   assert(lhs != nullptr);
   assert(rhs != nullptr);
